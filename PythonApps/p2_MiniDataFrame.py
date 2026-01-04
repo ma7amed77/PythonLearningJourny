5cc941b36b0c262c2540e577e1d09a82db32b59e
Mohamed Moataz <motazami111@gmail.com>
2026-01-04 07:52:37 +0200

    Added Project 2

---
+++ PythonApps/p2_MiniDataFrame.py
@@ -0,0 +1,72 @@
+def load_CSV(file_path):
+    with open(file_path, 'r') as f:
+        header = f.readline().strip().split(',')
+        def key_func(value):
+            try:
+                return float(value)
+            except ValueError:
+                return value
+        # zip creates a tuple of (header, value) pairs for each line
+        # dict casts them as (key, value)
+        data = [dict(zip(header, [key_func(val) for val in line.strip().split(',')])) for line in f.readlines() if line]
+    return data
+
+def filter_data(data, column, value):
+    return [row for row in data if row[column] == value]
+
+def sort_data(data, column):
+    sorted_rows = sorted(data, key = lambda r:r[column])
+    return sorted_rows
+
+#Here I return a column if no index provided or a dict if there is one
+def select_column(data, column, indicies = None):
+    if not indicies:
+        return [x[column] for x in data]
+    else:
+        return [{"id":x[indicies],"value":x[column]} for x in data]
+
+def get_Mean(data, column):
+    calc_column = select_column(data, column)
+    mean = 0 
+    try:
+        mean = sum(calc_column)/len(calc_column)
+    except Exception as e:
+        print(e)
+        return None
+    return mean
+
+#older implemntation
+def group_count(data, column):
+    result = {}
+    for row in data:
+        if row[column] in result:
+            result[row[column]] += 1
+        else:
+            result[row[column]] = 1
+    return result
+
+#currently I don't know what does group by exactly do
+#but this is more for learning data structer so it's good for now
+
+class Group:
+    def __init__(self, data, columns):
+        self.group = {}
+        groupKies = [row[columns[0]] for row in data]
+        groupValues = columns[1:]
+        self.group = {key:[] for key in set(groupKies)}
+        for row in data:
+            self.group[row[columns[0]]].append({x:row[x] for x in groupValues})
+    def get_sum(self, column):
+       return { groupedKey:sum([x[column] for x in groupedValDics]) for groupedKey, groupedValDics in self.group.items() }
+
+    def get_mean(self, column):
+        sum = self.get_sum(column)
+        return {k:sum[k]/len(self.group[k]) for k in self.group}
+
+
+# Testing
+data = load_CSV('data/testCSV.csv')
+#print(select_column(sort_data(data, "name"),'name')) ##print column of sorted Names
+#print(get_Mean(data,"age")) print mean of age
+#print(Group(data, ['city','age', 'name']).group) #print grouped data
+print(Group(data, ['city','age']).get_mean('age')) #Get mean age for each city
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-##### Hello, I am Mohamed Moataz a data science student at Alexandria University.
-##### Here I am sharing projects of me learning Python to master it.
+#### Hello, I am Mohamed Moataz a data science student at Alexandria University.
+#### Here I am sharing projects of me learning Python to master it.
+
+## P2 Mini DataFram
+A simple pandas like dataframe.
+Still has a lot to improve, like making DataFrame a class and make functions inside it, instead of being inside the main app. But this is for getting the hang of python data structures.
 
 ## P1 Simple Text Editor.
 In this projects I am practicing stacks and error handling.
